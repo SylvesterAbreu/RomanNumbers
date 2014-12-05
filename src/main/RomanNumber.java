abf1b570a13910df8dd8606c73be19d1f45688f1
Sylvester Abreu <sylvester.abreu@googlemail.com>
2014-12-05 17:50:38 +0000

    Refactoring duplication

--- src/main/RomanNumber.java
+++ src/main/RomanNumber.java
@@ -1,13 +1,20 @@
 package main;
 
 public class RomanNumber {
+
+    public static final String I = "I";
+    public static final String II = I + I;
+    public static final String III = II + I;
+    public static final String V = "V";
+    public static final String IV = I + V;
+
     public String getRomanNumberFor(int i){
         switch (i){
-            case 1: return "I";
-            case 2: return "II";
-            case 3: return "III";
-            case 4:return "IV";
-            default:return "V";
+            case 1: return I;
+            case 2: return II;
+            case 3: return III;
+            case 4:return IV;
+            default:return V;
         }
     }
 }
