31ff8a3ea26b77161e51f095b86cda0d68e8156c
Sylvester Abreu <sylvester.abreu@googlemail.com>
2014-12-08 09:19:32 +0000

    Refactoring numbers into a hash map as it might get easier in the near future as a place to store all these values

--- src/main/RomanNumber.java
+++ src/main/RomanNumber.java
@@ -1,6 +1,15 @@
 package main;
 
+import java.util.HashMap;
+
 public class RomanNumber {
+
+    static {
+        romanNumberHashMap = new HashMap<Integer, String>();
+        initializeRomanNumberHashMap();
+    }
+
+    private static final HashMap<Integer, String> romanNumberHashMap;
 
     private static final String I = "I";
     private static final String II = I + I;
@@ -13,32 +22,22 @@ public class RomanNumber {
     private static final String X = "X";
     private static final String IX = I + X;
 
-    public String getRomanNumberFor(int i) {
-        return i < 11 ? lessThanEleven(i) : "XI";
+    private static void initializeRomanNumberHashMap() {
+        romanNumberHashMap.put(new Integer(1), I);
+        romanNumberHashMap.put(new Integer(2), II);
+        romanNumberHashMap.put(new Integer(3), III);
+        romanNumberHashMap.put(new Integer(4), IV);
+        romanNumberHashMap.put(new Integer(5), V);
+        romanNumberHashMap.put(new Integer(6), VI);
+        romanNumberHashMap.put(new Integer(7), VII);
+        romanNumberHashMap.put(new Integer(8), VIII);
+        romanNumberHashMap.put(new Integer(9), IX);
+        romanNumberHashMap.put(new Integer(10), X);
     }
 
-    private String lessThanEleven(int i) {
-        switch (i) {
-            case 1:
-                return I;
-            case 2:
-                return II;
-            case 3:
-                return III;
-            case 4:
-                return IV;
-            case 5:
-                return V;
-            case 6:
-                return VI;
-            case 7:
-                return VII;
-            case 8:
-                return VIII;
-            case 9:
-                return IX;
-            default:
-                return X;
-        }
+    public String getRomanNumberFor(int i) {
+        final Integer input = new Integer(i);
+        final boolean hasInput = romanNumberHashMap.containsKey(input);
+        return hasInput ? romanNumberHashMap.get(input) : "XI";
     }
 }
